dd16ffe5803b404693aa350c75ba2642f390a856
leticiabrt <britotpleticia@gmail.com>
2024-07-23 16:31:25 -0300

    tudo errado

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('content')
+<div class="fundo">
+    <div class="jumbotron nosso">
+        <h1 class="display-4">Seja bem-vindo(a) ao nosso sistema de empréstimo de livros entre amigos.</h1>
+        <p class="lead">Aqui você pode visualizar os livros disponíveis e os membros do sistema.</p>
+        <hr class="my-4">
+        
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<img src="{!! asset('img/home.png')!!}" />
+
+                <a class="navbar-brand textoMenu" style="font-size: 40px; padding-left: 10px" href="#">Biblioteca</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+             
+              </div>
+</nav>
